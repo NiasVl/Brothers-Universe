18acbd01f364e01aef7a232ddeedfafb71324cf1
NiasVl <nicovieira88@gmail.com>
2025-06-17 16:42:19 -0300

    First commit

---
+++ src/data.js
@@ -0,0 +1,42 @@
+const playstation = [
+    {
+    nome: "The Last of Us Part II",
+    avaliacao: "3.1",
+    desc: " Um jogo emocionante de ação e aventura que continua a história de Ellie em um mundo pós-apocalíptico.",
+    preco: "R$349,99",
+    quant: "88",
+    img: ""
+    },
+    {
+    nome: "Spider-Man: Miles Morales",
+    avaliacao: "4.8",
+    desc: "Uma aventura incrível do Homem-Aranha, onde você controla Miles Morales e explora uma Nova York vibrante.",
+    preco: "R$299,99",
+    quant: "85",
+    img: ""
+    },
+    {
+    nome: "Demon's Souls (Remake)",
+    avaliacao: "3.5",
+    desc: "Uma versão renovada do clássico RPG de ação, conhecido por seu desafio e atmosfera sombria.",
+    preco: "R$349,99",
+    quant: "83",
+    img: ""
+    },
+    {
+    nome: "Ghost of Tsushima",
+    avaliacao: "4.2",
+    desc: "Um jogo de ação e aventura ambientado no Japão feudal, onde você assume o papel de um samurai lutando pela liberdade.",
+    preco: "R$299,99",
+    quant: "81",
+    img: ""
+    },
+    {
+    nome: "Horizon Forbidden West",
+    avaliacao: "4.5",
+    desc: "Uma aventura de mundo aberto com uma protagonista forte, explorando um mundo futurista cheio de criaturas incríveis.",
+    preco: "R$249,99",
+    quant: "77",
+    img: "https://m.media-amazon.com/images/I/61KU9GYqDPL._AC_UF1000,1000_QL80_.jpg"
+    }
+]
